a7043bd33cb314cb8c2ea808e6d133966dca6eeb
potatostore <a0105775135976@gmail.com>
2026-07-21 11:52:36 +0900

    daeco demo-service

---
+++ backend/demo-service/src/test/java/daeco/demo_service/DemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package daeco.demo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
